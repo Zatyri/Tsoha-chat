0f98ff33a16151881f6c328e2a8cd8d467653b9c
Oskar <gustosk@gmail.com>
2022-09-13 18:54:40 +0300

    Added basic registration template

--- routes.py
+++ routes.py
@@ -1,6 +1,38 @@
 from app import app
-from flask import render_template
+from flask import render_template, session, request, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 @app.route("/")
-def index():   
+def index():        
+    if 'username' in session:
+        id_token = session['username']        
     return render_template("index.html")
+
+@app.route('/login',methods=["POST"])
+def login():
+    if 'username' in session:
+        del session['username']
+    username = request.form['username']
+    password = request.form['password']
+    hash_value = generate_password_hash(password)
+
+    # TODO: check username and password
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/register")
+def register():
+    return render_template("register.html") 
+
+@app.route("/register/me" , methods=["POST"])
+def registerMe():
+    username = request.form['username']
+    password = request.form['password']
+    passwordVerification = request.form['passwordVerification']
+    return redirect("/")
