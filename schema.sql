122d53b7ed706fe4915492cb26485cf3cb209c8a
Oskar <gustosk@gmail.com>
2022-09-17 20:19:59 +0300

    added new tables to schema

--- schema.sql
+++ schema.sql
@@ -3,6 +3,31 @@ CREATE TABLE IF NOT EXISTS users
     id SERIAL NOT NULL PRIMARY KEY,
     username character varying NOT NULL,
     password character varying NOT NULL,
-    uuid character varying NOT NULL,
+    uuid character varying NOT NULL UNIQUE,
     role integer NOT NULL DEFAULT 1    
-)
+);
+CREATE TABLE IF NOT EXISTS rooms
+(
+  id SERIAL NOT NULL PRIMARY KEY,
+  creator character varying NOT NUll,
+  isPrivate boolean NOT NUll DEFAULT true  
+);
+CREATE TABLE IF NOT EXISTS usersInRoom
+(
+  room integer references rooms(id),
+  userID integer references users(id)
+);
+CREATE TABLE IF NOT EXISTS messages
+(
+  id SERIAL NOT NULL PRIMARY KEY,
+  author character varying references users(uuid),
+  room integer references rooms(id),
+  content character varying,
+  likes integer DEFAULT 0,
+  postedTime timestamp 
+);
+CREATE TABLE IF NOT EXISTS repliedMessages
+(
+  parentMessage integer references messages(id),
+  childMessage integer references messages(id)
+);
